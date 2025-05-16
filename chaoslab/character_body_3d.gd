a62153180232c455842e2d3cebb44c4d8e7dae3b
Ossi <felix.nutz98@web.de>
2025-05-16 23:07:08 +0200

    Grid Map Collision Fix

--- chaoslab/character_body_3d.gd
+++ chaoslab/character_body_3d.gd
@@ -7,19 +7,14 @@ func _physics_process(delta: float) -> void:
 
 	inputVector = Vector2.ZERO
 	if Input.is_action_pressed("Input_A"):
-		print("LEFT")
 		inputVector.y -= 1
 	if Input.is_action_pressed("Input_D"):
-		print("RIGHT")
 		inputVector.y += 1
 	if Input.is_action_pressed("Input_W"):
-		print("UP")
 		inputVector.x += 1
 	if Input.is_action_pressed("Input_S"):
-		print("DOWN")
 		inputVector.x -= 1
 		
 	inputVector = inputVector.normalized()
 	velocity = Vector3(inputVector.x,0,inputVector.y)*movementSpeed
-	
 	move_and_slide()
